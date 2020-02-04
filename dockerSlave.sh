91cbb6a36fd5e3e0df189734c665d091128222b3
balaganur <59176232+balaganur@users.noreply.github.com>
2020-02-04 07:38:50 +0530

    update slave

--- dockerSlave.sh
+++ dockerSlave.sh
@@ -6,4 +6,5 @@ sudo apt install -y curl
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt install -y kubeadm
 sudo swapoff -a
-sudo hostnamectl set-hostname slave-node
+sudo hostnamectl set-hostname slave-1
+sudo kubeadm join 172.31.11.225:6443 --token dh320x.ls67jdi0cds4omjd --discovery-token-ca-cert-hash sha256:28df858e8fb5669932a866f4ef877deaf2466449aac98ae08ee930bfcd1829e6
